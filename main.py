2d279caa30ba416ef8832049cdaa174b54064668
Nem T. Lea <nemtielea@gmail.com>
2024-04-10 15:20:40 +0200

    dodanie pliku glownego z notatkami

---
+++ main.py
@@ -0,0 +1,15 @@
+#print("Hello Future Collars!")
+imie = "Adam"
+tekst_przywitania = "Hello "
+#print(tekst_przywitania + imie + "!")
+
+zmienna_tekstowa = "tekst w 'cudzyslowach, Let's go!"
+zmienna_tekstowa_2 = 'tekst w "apostrofach"'
+
+pozdrowienie_w_wielu_liniach = """Hej!
+Co u Ciebie slychac?
+Pozdrawiam!"""
+
+print(zmienna_tekstowa)
+print(zmienna_tekstowa_2)
+print(pozdrowienie_w_wielu_liniach)
