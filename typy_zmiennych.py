c803dacd611d6e64d13c88c9c2091d27fff99942
Nem T. Lea <nemtielea@gmail.com>
2024-04-11 09:19:40 +0200

    notatki z typow zmiennych

---
+++ typy_zmiennych.py
@@ -0,0 +1,24 @@
+# Teksty - string
+tekst = "To jest nasza zmienna tekstowa"    #str
+print(tekst)
+
+# Liczby calkowite - integer
+liczba_calkowita = 123      # int
+print(liczba_calkowita)
+
+# Liczby zmiennoprzecinkowe - float
+liczba_zmiennaprzecinkowa = 10.5        #float
+print(liczba_zmiennaprzecinkowa)
+liczba_zmiennaprzecinkowa_2 = 10.0      #float
+print(liczba_zmiennaprzecinkowa_2)
+
+# Zmienna logiczna
+prawda = True
+falsz = False
+print(prawda)
+print(falsz)
+
+# Zmienna "pusta"
+pusta_zmienna = None
+
+
