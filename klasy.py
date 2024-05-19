e0e080e64dcd0ef967303e1899dac49a04994601
Nem T. Lea <nemtielea@gmail.com>
2024-05-19 22:44:51 +0200

    poprawienie przykladu paliwa

--- klasy.py
+++ klasy.py
@@ -8,30 +8,32 @@
 
 
 class Samochod:
-    def __init__(self, marka, kolor, model):
+    def __init__(self, marka, kolor, model, max_poziom_paliwa, przyciemniane_szyby=False):
         self.marka = marka
         self.kolor = kolor
         self.model = model
-        self.poziom_paliwo = 0
+        self.poziom_paliwa = 0
+        self.przycemniane_szyby = przyciemniane_szyby
+        self.max_poziom_paliwa = max_poziom_paliwa
 
     def wyswietl_parametry(self):
         print(f"Marka: {self.marka} | Model: {self.model} | Kolor: {self.kolor}")
 
     def dolej_paliwo(self, liczba_litrow):
-        self.poziom_paliwo += liczba_litrow
+        if self.poziom_paliwa + liczba_litrow > self.max_poziom_paliwa:
+            print("Przelewa sie!")
+            self.poziom_paliwa = self.max_poziom_paliwa
+        else:
+            self.poziom_paliwa += liczba_litrow
 
     def wyswietl_poziom_paliwa(self):
-        print(f"W baku jest {self.poziom_paliwo}l paliwa.")
+        print(f"W baku jest {self.poziom_paliwa}l paliwa.")
 
 
-fiat_500 = Samochod(marka='Fiat', kolor='czerwony', model='500')
+fiat_500 = Samochod(marka='Fiat', kolor='czerwony', model='500', max_poziom_paliwa=35)
 fiat_500.wyswietl_poziom_paliwa()
 fiat_500.dolej_paliwo(5)
 fiat_500.wyswietl_poziom_paliwa()
-
-
-
-
 
 
 # mazda_mx5 = Samochod(marka='Mazda', kolor='czarny', model='MX-5')
@@ -40,4 +42,3 @@ fiat_500.wyswietl_poziom_paliwa()
 # for s in [fiat_500, mazda_mx5]:
 #     # print(f"{s.marka} koloru {s.kolor}")
 #     s.wyswietl_parametry()
-
