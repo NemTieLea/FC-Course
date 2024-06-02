016e951528e1503a7d15c1b272857fc02e27b5be
Nem T. Lea <nemtielea@gmail.com>
2024-06-02 16:56:26 +0200

    dodano notatki

--- pliki.py
+++ pliki.py
@@ -1,5 +1,27 @@
-plik = open("test.txt")
+from helpers import zamien_przecinki_na_spacje
 
-zawartosc = plik.read().strip
+# plik = open("test.txt")
+# zawartosc_pliku = plik.readlines()
+# plik.close()
 
-print(f"[{zawartosc}]")
+with open("test.txt", "r") as plik:
+    # zawartosc_pliku = plik.read().strip()
+    zawartosc_pliku = []
+    for linia in plik:
+        zawartosc_pliku.append(linia.strip())
+
+print(zawartosc_pliku)
+
+# dane_do_zapisu = []
+# for linia in zawartosc_pliku:
+#     dane_do_zapisu.append(
+#         zamien_przecinki_na_spacje(linia) + "\n"
+#     )
+#
+# with open("nowy_tekst.txt", "w") as plik:
+#     plik.writelines(dane_do_zapisu)
+#
+# with open("nowy_tekst_2.txt", "w") as plik:
+#     for linia in zawartosc_pliku:
+#         nowa_linia = zamien_przecinki_na_spacje(linia) + "\n"
+#         plik.write(nowa_linia)
